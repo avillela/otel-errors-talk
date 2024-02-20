6eced0ad6585da19595b1c702fb9232851a9aa59
Adriana Villela <indycomp0t1@gmail.com>
2024-02-20 18:55:19 -0500

    Clean up log messages in server.py

--- src/python/server.py
+++ src/python/server.py
@@ -9,16 +9,16 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 @app.route("/rolldice")
 def roll_dice():
-    logging.getLogger().debug("WTF DUDE")
+    logging.getLogger().debug("This is a log message associated to an auto-instrumented span.")
     res = ""
     try:
         res = str(do_roll())
     except Exception as e:
         res = "0"
-        with tracer.start_as_current_span("odd_number") as span:
+        with tracer.start_as_current_span("even_number") as span:
             span.record_exception(e)
             print(f"PRINT: Returning value {res} from exception {str(e)}")
-            logging.getLogger().error("Uh-oh. We have an exception")
+            logging.getLogger().error("Uh-oh. We have an exception. This log message only prints if this span is created. The span is only created if an exception is thrown.")
         
     return res
 
@@ -29,7 +29,6 @@ def do_roll():
     with tracer.start_as_current_span("do_roll", set_status_on_exception=True) as span:
         span = trace.get_current_span()
         span.set_attribute("roll.value", res)
-        # span.set_status(trace.StatusCode.ERROR)
         
         # Add attributes for span event
         attributes = {}
@@ -38,7 +37,7 @@ def do_roll():
 
         span.add_event("This is a span event", attributes=attributes)
 
-        logging.getLogger().info("This is a log message!!")
+        logging.getLogger().info("This is a log message! The span may err out, but this log message is independent of whether or not the span errs out.")
 
         request_counter.add(1)
         
